01042b3122e8d802501e35bac374318537a21084
batake_ <batake25@gmail.com>
2024-07-17 08:53:45 +0900

    佐藤さんと相談しながらinput周りの改善

--- src/dashboard/Panel.tsx
+++ src/dashboard/Panel.tsx
@@ -1,135 +1,139 @@
-import React,{ useState } from 'react';
+import React,{ useCallback, useEffect, useMemo, useState } from 'react';
 import { useReplicant } from "@nodecg/react-hooks";
-import { GameInfoData } from '../types/schemas/gameInfoData';
+import { GameInfo, GameInfoData } from '../types/schemas/gameInfoData';
 
 interface GamePanelProps{}
 
-export const gameInfoData = nodecg.Replicant<GameInfoData>('gameInfo');
+// replicantの実態
+//export const aaa = nodecg.Replicant<GameInfoData>('gameInfo');
 
 export function GamePanel(){
 
+	// 関数の実行した結果をキャッシュしてくれる(インスタンスをキャッシュ)
+	// 空配列なら
+	//const rep = useMemo(()=>{
+	//	return nodecg.Replicant<GameInfoData>('gameInfo');
+	//}, []);
+
 	const playerCount = 4;
+
+	const [gameName, setGameName] = useState<string>("");
+	const [players, setPlayers] = useState<string[]>(["","","",""]);
+	const [categories, setCategorys] = useState<string[]>(["","","",""]);
+	const [estimatedTime, setEstimateTime] = useState<string>("");
+
+	// 非同期で返ってくる
 	const [gameInfo, setGameInfo] = useReplicant<GameInfoData>('gameInfo', {
 		bundle: "nodecg-layout-practice",
-		defaultValue: {
-			gameName:'',
-			players: ['','','',''],
-			categories:['','','',''],
-			estimatedTime:''
-		}
 	});
-	if(gameInfo === undefined) return;
 
-	if(gameInfo.players.length < playerCount) {
-		setGameInfo({...gameInfo, players:[gameInfo.players[0],gameInfo.players[1],gameInfo.players[2],gameInfo.players[3]]})
-	}
-	if(gameInfo.categories.length < playerCount){
-		setGameInfo({...gameInfo, categories:[gameInfo.categories[0],gameInfo.categories[1],gameInfo.categories[2],gameInfo.categories[3]]})
-	}
-
-	/*
-	const [tempGameInfo, setTempGameInfo] = useState<GameInfoData>({
-		gameName:'',
-		players: ['','','',''],
-		categories:['','','',''],
-		estimatedTime:''}
-	);
-	*/
-	/*
-	// changeイベントの発行
+	// あんまり使いたくない
+	// 外部のオブジェクトなどを参照している場合は自身でreturnで破棄しないといけない
 	useEffect(()=>{
-		gameInfoReplicant.on('change', (newValue: GameInfoData | undefined) =>{
-			if(newValue){
-				setGameInfo(newValue);
-			}
-		});
-	}, []);
-	*/
-
+		setGameName(gameInfo?.gameName ?? "");
+		setPlayers(gameInfo?.players ?? []);
+		setCategorys(gameInfo?.categories ?? []);
+		setEstimateTime(gameInfo?.estimatedTime ?? "");
+	}, [gameInfo]);
+	
 	// プレイヤー名変更
 	const handlePlayerChange = (index:number, value: string) => {
-		const newPlayers = [...gameInfo.players];
+		if(players === undefined) return;
+
+		// 直接渡すと参照私になってしまうので、新しい配列を作成する
+		const newPlayers = [...players];
 		newPlayers[index] = value;
-		setGameInfo({...gameInfo, players: newPlayers});
+		setPlayers(newPlayers);
 	}
 
 	// カテゴリー名変更
 	const handleCategoryChange = (index:number, value:string) => {
-		const newCategories = [...gameInfo.categories];
+		if(categories === undefined) return;
+
+		const newCategories = [...categories];
 		newCategories[index] = value;
-		setGameInfo({...gameInfo, categories: newCategories});
+		setCategorys(newCategories);
 	}
 
 	// 確定
 	const handleSubmit = (e: React.FormEvent<HTMLFormElement>)=>{
-		e.preventDefault();
+		if(gameInfo === undefined) return;
 
+		e.preventDefault();
 		const updateGameInfo: GameInfoData = {
 			...gameInfo,
-			players: gameInfo.players.filter(player => player != ''),
-			categories: gameInfo.categories.filter(category => category != ''),
+			gameName:gameName ?? "",
+			players: (players?.filter(player => player != '')) ?? ["","","",""],
+			categories: (categories?.filter(category => category != '')) ?? ["","","",""],
+			estimatedTime: estimatedTime ?? ""
 		};
 
 		setGameInfo(updateGameInfo);
 		console.log('gameInfoUpdate!:', updateGameInfo);
 	};
 
-	return (
-		<div className="game-panel">
-		  <h2>Game Information</h2>
-		  <hr/>
-		  <form onSubmit={handleSubmit}>
-			<div className="form-group">
-			  <label htmlFor="gameName">Game Name:</label><br></br>
-			  <input
-				type="text"
-				id="gameName"
-				value={gameInfo.gameName}
-				onChange={(e) => setGameInfo({ ...gameInfo, gameName: e.target.value })}
-			  />
-			</div>
-			<hr/>
-			<div className="form-group">
-			  <label>Players:</label><br></br>
-			  {gameInfo.players.map((player, index) => (
+	const renderEditor = () => {
+		if(gameInfo === undefined) return;
+
+		return (
+			<div className="game-panel">
+			  <h2>Game Information</h2>
+			  <hr/>
+			  <form onSubmit={handleSubmit}>
+				<div className="form-group">
+				  <label htmlFor="gameName">Game Name:</label><br></br>
+				  <input
+					type="text"
+					id="gameName"
+					value={gameName}
+					onChange={(e) => setGameName(e.target.value)}
+				  />
+				</div>
+				<hr/>
+				<div className="form-group">
+				  <label>Players:</label><br></br>
+				  {[...Array(playerCount).keys()].map(index => (
+					<input
+					  key={index}
+					  type="text"
+					  value={players[index] ?? ""}
+					  onChange={(e) => handlePlayerChange(index, e.target.value)}
+					  placeholder={`Player ${index + 1}`}
+					/>
+				  ))}
+				</div>
+				<hr/>
+				<div className="form-group">
+				<label>Categories:</label><br></br>
+				{[...Array(playerCount).keys()].map(index => (
+					<input
+					key={index}
+					type="text"
+					value={categories[index] ?? ""}
+					onChange={(e) => handleCategoryChange(index, e.target.value)}
+					placeholder={`Category ${index + 1}`}
+					/>
+				))}
+				</div>
+				<hr/>
+				<div className="form-group">
+				<label htmlFor="estimatedTime">Estimated Time:</label><br></br>
 				<input
-				  key={index}
-				  type="text"
-				  value={player}
-				  onChange={(e) => handlePlayerChange(index, e.target.value)}
-				  placeholder={`Player ${index + 1}`}
+					type="text"
+					id="estimatedTime"
+					value={estimatedTime}
+					onChange={(e) => setEstimateTime(e.target.value)}
+					placeholder="HH:MM:SS"
 				/>
-			  ))}
-			</div>
-			<hr/>
-			<div className="form-group">
-			<label>Categories:</label><br></br>
-			{gameInfo.categories.map((category, index) => (
-				<input
-				key={index}
-				type="text"
-				value={category}
-				onChange={(e) => handleCategoryChange(index, e.target.value)}
-				placeholder={`Category ${index + 1}`}
-				/>
-			))}
-			</div>
-			<hr/>
-			<div className="form-group">
-			<label htmlFor="estimatedTime">Estimated Time:</label><br></br>
-			<input
-				type="text"
-				id="estimatedTime"
-				value={gameInfo.estimatedTime}
-				onChange={(e) => setGameInfo({ ...gameInfo, estimatedTime: e.target.value })}
-				placeholder="HH:MM:SS"
-			/>
-			</div>
-			<br></br>
-        <button type="submit">Update</button>
-      </form>
-    </div>
-  );
+				</div>
+				<br></br>
+			<button type="submit">Update</button>
+		  </form>
+		</div>
+	  );
+	}
+
+	return renderEditor();
 };
 
 export function Panel() {
